0d542b09294767f63cdd44d1fea4a8919623462a
nahuelhsouto <nahuelhsouto@gmail.com>
2020-12-12 21:07:01 -0300

    Descr

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load',()=>{
 
 let long;
 let lat;
-let tempDescrip=document.querySelector(".temp-decription");
+let tempDescrip=document.querySelector(".temp-description");
 let tempGrado=document.querySelector(".temp-grados");
 let tempZona=document.querySelector(".Zona");
 let celGrade;
@@ -26,11 +26,13 @@ fetch(api)
    const {temp}=data.main
    name=data.name
    weather.iconId=data.weather[0].icon;
+   weather.temp=data.weather[0].description;
    country=data.sys.country
    celGrade=Math.floor(temp-273,15)
    
 tempZona.textContent=`${country}, ${name}`;
 tempGrado.textContent=`${celGrade}°c`;
+tempDescrip.innerHTML=`${weather.temp}`;
 tempIcon.innerHTML=`<img src="icons/${weather.iconId}.png"/>`;
 })
     });
